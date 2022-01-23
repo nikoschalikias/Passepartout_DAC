e20edcac6c6ec0b667b9ede434278e616b794066
nikos chalikias <nikos.chalikias@gmail.com>
2022-01-23 11:16:35 +0000

    nikos update for passepartout

--- main.cpp
+++ main.cpp
@@ -1,72 +1,40 @@
+//nikos: 
+// imported from: https://os.mbed.com/teams/fra221-pb11/code/Lab4MCP492_SPI//file/b81dc534e7d7/main.cpp/
+// references: https://www.arduino.cc/reference/en/libraries/mcp_dac/
+//             https://github.com/RobTillaart/MCP_DAC
+//             https://os.mbed.com/teams/FRA221_2015/code/MCP4922_Sinewave/file/bf75b2a7d8b5/MCP4922.h/
+//             https://ww1.microchip.com/downloads/en/DeviceDoc/22244B.pdf
+//             https://ww1.microchip.com/downloads/en/devicedoc/22250a.pdf
+
 #include "mbed.h"
 #include "math.h"
-/**********************
- *      MCP4922
- **********************/
 
-///SPI Interface
-SPI MCP4922_SPI(D11,D12,D13);//MOSI ,MISO ,SCK
-DigitalOut MCP4922_CS(D10);//CS
+SPI MCP4922_SPI(PA_12,PA_6,PA_5);//MOSI ,MISO ,SCK  // nikos:  D12 MISO PA_6 NOT USED IN DAC, BUT IS USED AS VALVE2 in Passepartout motherboard
+DigitalOut MCP4922_CS(PA_11);//CS
+DigitalIn mode_s(PB_2); // nikos: #TEC-DAC-LDAC, WAS D8
+DigitalOut MCP4922_LDAC(PB_2);  // nikos: #TEC-DAC-LDAC, WAS D8
+DigitalOut MCP4922_SHDN(PD_15);    // nikos: SHDN is connected to +3.3V in Passepartout motherboard, D9 is PD_15, is not connected
 
-AnalogIn knob(A1);
-
-DigitalIn mode_s(D8);
-
-//Digital Output
-DigitalOut MCP4922_LDAC(D8);
-DigitalOut MCP4922_SHDN(D9);
-void volt_out(double volt)
-{
-
+void volt_out(double volt) {
     uint16_t temp=((uint16_t)(volt*4096/3.33))&0x0fff;
     uint16_t MCP4922_Sent_Data = 0;
     MCP4922_Sent_Data = 0xB000|temp;
-    //MCP4922_Sent_Data = 0x3fff;
-    MCP4922_CS = 0;
-
-
-    //sent data buffer to mcp4922
-    MCP4922_SPI.write(MCP4922_Sent_Data);
-
-    //set CS to High to End MCP4922 SPI Interface
-    MCP4922_CS = 1;
-
-    //set LDAC to LOW to Load data to output
-    MCP4922_LDAC = 0 ;
-
-    //wait at least 100ns = 0.1 microsec ..... So set at 1 microsec
-    wait_us(1);
-
-    //set LDAC to High to prepare next output
-    MCP4922_LDAC = 1 ;
+    MCP4922_CS = 0;  //MCP4922_Sent_Data = 0x3fff;
+    MCP4922_SPI.write(MCP4922_Sent_Data); //sent data buffer to mcp4922
+    MCP4922_CS = 1;  //set CS to High to End MCP4922 SPI Interface
+    MCP4922_LDAC = 0 ;  //set LDAC to LOW to Load data to output
+    wait_us(1);  //wait at least 100ns = 0.1 microsec ..... So set at 1 microsec
+    MCP4922_LDAC = 1 ; //set LDAC to High to prepare next output
 }
-int main()
-{
-    /******************************
-     *****      setup       *******
-     ******************************/
-    uint16_t volt_temp=0;
-    /////////setup SPI//////////
-    //set CS High
+
+int main() {  
     MCP4922_CS=1;
-
-    //set SPI mode [1,1]=> mode 3 @16bit
     MCP4922_SPI.format(16,3);
-
-    //set SPI Frequency @20MHz
     MCP4922_SPI.frequency(1000000);
-
-    //set Load DAC High (inactive)
     MCP4922_LDAC = 1;
-
-    //set shutdown DAC High (inactive)
     MCP4922_SHDN = 1;
 
-    //loop
     while(1) {
-             /* if(mode_s)volt_temp=uint16_t(3.33-knob.read()*3.33);
-              else volt_temp=uint16_t(knob.read()*3.33);
-              volt_out(volt_temp); */
-              volt_out(2.5);
-           }
+        volt_out(1);
+    }
 }
